5ec2b0e8f2e8bf760bcd397590447b4235155a17
m <m@bitsnbites.eu>
2017-10-01 10:36:03 +0200

    Add a "Stars" preset

--- presets.js
+++ presets.js
@@ -292,6 +292,39 @@ gInstrumentPresets = [
     ]
   },
   {
+    name: "Stars",
+    i: [
+          2, // OSC1_WAVEFORM
+          138, // OSC1_VOL
+          116, // OSC1_SEMI
+          0, // OSC1_XENV
+          2, // OSC2_WAVEFORM
+          138, // OSC2_VOL
+          128, // OSC2_SEMI
+          4, // OSC2_DETUNE
+          0, // OSC2_XENV
+          0, // NOISE_VOL
+          47, // ENV_ATTACK
+          48, // ENV_SUSTAIN
+          107, // ENV_RELEASE
+          124, // ARP_CHORD
+          3, // ARP_SPEED
+          0, // LFO_WAVEFORM
+          139, // LFO_AMT
+          4, // LFO_FREQ
+          1, // LFO_FX_FREQ
+          3, // FX_FILTER
+          64, // FX_FREQ
+          160, // FX_RESONANCE
+          3, // FX_DIST
+          32, // FX_DRIVE
+          147, // FX_PAN_AMT
+          4, // FX_PAN_FREQ
+          121, // FX_DELAY_AMT
+          5 // FX_DELAY_TIME
+    ]
+  },
+  {
     name: "====[DRUMS]===="
   },
   {
