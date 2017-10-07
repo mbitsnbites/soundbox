2aabcd72d601f7ad474d4592e6e5bdd3f355c030
Veikko Sariola <veikko@andrew.cmu.edu>
2017-10-07 18:00:40 +0300

    Ask user to confirm "new", "load" and leaving page if there's unsaved changes
    Stores a deep copy of the song in unmodified state, and does a deep equality comparison
    between the unmodified and current song to see if there's need to confirm

--- common.js
+++ common.js
@@ -31,3 +31,56 @@ function include(filename)
 	head.appendChild(s);
 }
 
+// based on https://j11y.io/javascript/deep-copying-of-objects-and-arrays/
+function deepCopy(obj)
+{
+  if (Object.prototype.toString.call(obj) === '[object Array]') {
+    var out = [], i = 0, len = obj.length;
+    for ( ; i < len; i++ ) {
+      out[i] = arguments.callee(obj[i]);
+    }
+    return out;
+  }
+  if (typeof obj === 'object') {
+    var out = {}, i;
+    for ( i in obj ) {
+      out[i] = arguments.callee(obj[i]);
+    }
+    return out;
+  }
+  return obj;
+}
+
+
+function deepEquals(a,b)
+{   
+  // if a and b are arrays, compare them as such
+  if (Object.prototype.toString.call(a) === '[object Array]' && 
+      Object.prototype.toString.call(b) === '[object Array]') {
+    if (a.length != b.length)
+      return false;
+    for (var i=0;i < a.length; i++ ) {
+      if (!arguments.callee(a[i],b[i]))
+        return false;
+    }
+    return true;
+  }
+  // if a and b are associative arrays, compare them as such
+  if (typeof a === 'object' && typeof b === 'object') {   
+    var checkKeys = function(a,b) {
+      for(key in a) //In a and not in b
+        if(!(key in b))
+          return false;
+      return true;
+    }
+    if (!checkKeys(a,b) || !checkKeys(b,a))
+      return false;
+    for (i in a) {
+      if (!arguments.callee(a[i],b[i]))
+        return false;
+    }
+    return true;
+  }   
+  return a === b;
+}
+
